aff991eab79844c7edcf2d6fbb2961ad4c691318
Luis Inacio <lrs.inacio@gmail.com>
2023-03-19 21:01:24 +0000

    first commit

---
+++ NavigationMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NavigationMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NavigationMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NavigationMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
